6779647ad17664a0d718fd471fe2c1e9be774462
saldatkin <aliaksei.saldatkin@gmail.com>
2022-09-11 15:46:26 +0300

    init: create new vilnius-attractions project

---
+++ angular-vilnius-attractions/src/app/main/osm-map/osm-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-osm-map',
+  templateUrl: './osm-map.component.html',
+  styleUrls: ['./osm-map.component.scss']
+})
+export class OsmMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
