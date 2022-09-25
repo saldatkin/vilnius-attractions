ef8ea17d6975fe2dc5c0153b3529c9f3edf84550
saldatkin <aliaksei.saldatkin@gmail.com>
2022-09-25 19:00:43 +0300

    feat: add header and icons for sights

--- angular-vilnius-attractions/src/main.js
+++ angular-vilnius-attractions/src/main.js
@@ -1,7 +1,1 @@
 
-const options = document.getElementsByTagName('option');
-console.log(options);
-for (let i = 0; i < options.length; i++) {
-  const element = options[i];
-  console.log(element.outerText);
-}
